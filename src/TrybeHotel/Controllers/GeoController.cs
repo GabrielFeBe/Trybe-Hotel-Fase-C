74d8c330451a2eee30c99d3e5a6d14432c636ca0
Gabriel Fernandes <hu3master.zord@hotmail.com>
2023-10-26 11:20:49 -0300

    req 11

--- src/TrybeHotel/Controllers/GeoController.cs
+++ src/TrybeHotel/Controllers/GeoController.cs
@@ -26,7 +26,9 @@ namespace TrybeHotel.Controllers
         [Route("status")]
         public async Task<IActionResult> GetStatus()
         {
-            throw new NotImplementedException();
+
+            var status = await _geoService.GetGeoStatus();
+            return Ok(status);
         }
 
         // 12. Desenvolva o endpoint GET /geo/address
--- src/TrybeHotel/Services/GeoService.cs
+++ src/TrybeHotel/Services/GeoService.cs
@@ -1,4 +1,5 @@
 using System.Net.Http;
+using System.Security.Policy;
 using TrybeHotel.Dto;
 using TrybeHotel.Repository;
 
@@ -6,18 +7,29 @@ namespace TrybeHotel.Services
 {
     public class GeoService : IGeoService
     {
-         private readonly HttpClient _client;
+        private readonly HttpClient _client;
+        private readonly string _baseUrl = "https://nominatim.openstreetmap.org/status.php?format=json";
         public GeoService(HttpClient client)
         {
             _client = client;
+            _client.DefaultRequestHeaders.Add("Accept", "application/json");
+            _client.DefaultRequestHeaders.Add("User-Agent", "aspnet-user-agent");
+            _client.BaseAddress = new Uri(_baseUrl);
         }
 
         // 11. Desenvolva o endpoint GET /geo/status
         public async Task<object> GetGeoStatus()
         {
-            throw new NotImplementedException();
+            var response = await _client.GetAsync("");
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("Não foi possível obter o status do serviço de geolocalização");
+            }
+
+            var result = await response.Content.ReadFromJsonAsync<object>();
+            return result;
         }
-        
+
         // 12. Desenvolva o endpoint GET /geo/address
         public async Task<GeoDtoResponse> GetGeoLocation(GeoDto geoDto)
         {
@@ -30,23 +42,25 @@ namespace TrybeHotel.Services
             throw new NotImplementedException();
         }
 
-       
 
-        public int CalculateDistance (string latitudeOrigin, string longitudeOrigin, string latitudeDestiny, string longitudeDestiny) {
-            double latOrigin = double.Parse(latitudeOrigin.Replace('.',','));
-            double lonOrigin = double.Parse(longitudeOrigin.Replace('.',','));
-            double latDestiny = double.Parse(latitudeDestiny.Replace('.',','));
-            double lonDestiny = double.Parse(longitudeDestiny.Replace('.',','));
+
+        public int CalculateDistance(string latitudeOrigin, string longitudeOrigin, string latitudeDestiny, string longitudeDestiny)
+        {
+            double latOrigin = double.Parse(latitudeOrigin.Replace('.', ','));
+            double lonOrigin = double.Parse(longitudeOrigin.Replace('.', ','));
+            double latDestiny = double.Parse(latitudeDestiny.Replace('.', ','));
+            double lonDestiny = double.Parse(longitudeDestiny.Replace('.', ','));
             double R = 6371;
             double dLat = radiano(latDestiny - latOrigin);
             double dLon = radiano(lonDestiny - lonOrigin);
-            double a = Math.Sin(dLat/2) * Math.Sin(dLat/2) + Math.Cos(radiano(latOrigin)) * Math.Cos(radiano(latDestiny)) * Math.Sin(dLon / 2) * Math.Sin(dLon / 2);
-            double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1-a));
+            double a = Math.Sin(dLat / 2) * Math.Sin(dLat / 2) + Math.Cos(radiano(latOrigin)) * Math.Cos(radiano(latDestiny)) * Math.Sin(dLon / 2) * Math.Sin(dLon / 2);
+            double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
             double distance = R * c;
-            return int.Parse(Math.Round(distance,0).ToString());
+            return int.Parse(Math.Round(distance, 0).ToString());
         }
 
-        public double radiano(double degree) {
+        public double radiano(double degree)
+        {
             return degree * Math.PI / 180;
         }
 
